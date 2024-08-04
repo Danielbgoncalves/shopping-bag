ce2eb687641b061383973ce3719105980868d9bc
Daniel Borges <44danbor@gmail.com>
2024-08-04 11:48:11 -0300

    favicons e cors

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,18 @@
 const express = require('express'); 
+const cors = require('cors')
+
 const app = express();              
 const port = 3001;
 
-let sacolaData = [];
+const corsOption = {
+    origin: 'http://127.0.0.1:5500',
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOption));
 app.use(express.json()); 
 
+let sacolaData = [];
 app.get('/api/sacola',(req, res)=> {   
     try{
         res.status(200).json(sacolaData); 
@@ -19,17 +27,17 @@ app.post('/api/sacola', (req, res)=> {
     try{
         const username = req.headers['username'];
         const sacola = req.body;
-        if(!sacola || Object.keys(sacola).length === 0){
-            return res.status(400).send('Dados da sacola ausentes');
+        if(!sacola || sacola.length === 0){ // ou seria   Object.keys(sacola).length === 0  ?
+            return res.status(400).json({mesage: 'Dados da sacola ausentes'});
         } 
         
         const sacolaComUsuario = {username, sacola};
         sacolaData.push(sacolaComUsuario);
         console.log(`sacola reebida ${sacola}`);
-        res.status(200).send('sacola recebida com sucesso');
+        res.status(200).json({message: 'sacola recebida com sucesso'});
     }catch (error){
         console.error('erro ao abrir sacola: ', error);
-        res.status(500).send('Erro do servidor ao processar sacola');
+        res.status(500).json({message: 'Erro do servidor ao processar sacola'});
 
     } 
 });
